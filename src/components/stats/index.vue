2e4420d9601e5951012acf32010b7d335e5f9f0e
Стас Штауб <47520675+stasshtaub@users.noreply.github.com>
2020-08-25 18:25:32 +0300

    feature/ добавил сообщение об отсутствии продаж

--- src/components/stats/index.vue
+++ src/components/stats/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="stats pa-4">
-    <stats-sales class="mt-8" :sales="stats.sales" />
-    <stats-sources class="mt-8" :sources="stats.sources" />
+    <template v-if="stats.sales.length && stats.sources.length">
+      <stats-sales class="mt-8" :sales="stats.sales" />
+      <stats-sources class="mt-8" :sources="stats.sources" />
+    </template>
+    <v-alert v-else type="info">
+      Нет продаж, удовлетворяющих фильтрам
+    </v-alert>
   </div>
 </template>
 
